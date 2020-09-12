3fccf26bf9d755b369556e38a2bcf6feab82e16b
amit.harjani <amit.harjani@freecharge.com>
2020-09-12 20:57:23 +0530

    change port

--- index.js
+++ index.js
@@ -56,6 +56,6 @@ const server = new ApolloServer({ typeDefs, resolvers , engine: {
   }});
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
 });
